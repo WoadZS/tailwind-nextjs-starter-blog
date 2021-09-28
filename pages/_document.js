a0f66b1a51c245d163bc16b5bc16363d471409a5
WoadZS <WoadZS@users.noreply.github.com>
2021-09-28 18:45:00 +0800

    bug fix

--- pages/_document.js
+++ pages/_document.js
@@ -28,7 +28,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
-          <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap"
+            rel="stylesheet"
+          />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
